7285719e8f51f6d464cf34423f99f1edd475233e
LogoStick <46106844+LogoStick@users.noreply.github.com>
2020-06-20 04:19:06 -0700

    First commitment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,14 @@ import { AppComponent } from './app.component';
 import {AbstractProductService} from './Services/ProductServices/AbstractProductService';
 import {ProductsFakeDBService} from './Services/ProductServices/products-fake-db.service';
 import {HttpClientModule} from '@angular/common/http';
-import { HomepageComponent } from './Components/homepage/homepage.component';
-import {AppRoutingModule} from './Modules/custom-router/app-routing.module';
-import { CartComponent } from './Components/Homepage/cart/cart.component';
-import { SearchboxComponent } from './Components/Homepage/searchbox/searchbox.component';
-import { ProductdisplayerComponent } from './Components/Homepage/productdisplayer/productdisplayer.component';
-import { DisplayedproductComponent } from './Components/Homepage/displayedproduct/displayedproduct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    CartComponent,
-    SearchboxComponent,
-    ProductdisplayerComponent,
-    DisplayedproductComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
   ],
   providers: [
     {
